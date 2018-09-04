8d2bc51bed1697addd37fe7711ec1f3c54958b17
Ana Laura <analaura.pace@gmail.com>
2018-09-04 09:39:47 -0300

    First commit

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,41 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void sum() {
+		System.out.println("sum test");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void substract() {
+		System.out.println("substract test");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void division() {
+		System.out.println("division test");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
+	}
+	
+	@Test
+	public void multiplication() {
+		System.out.println("multiplication test");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+}
+
